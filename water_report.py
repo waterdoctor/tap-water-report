57ccd592d332959ca06ed0d24009ba98fb06bc59
waterdoctor <123717100+waterdoctor@users.noreply.github.com>
2023-02-02 16:40:02 -0500

    bug fix

--- water_report.py
+++ water_report.py
@@ -30,7 +30,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("style.css")
+local_css("css/style.css")
 
 
 # -------- SETTINGS --------
