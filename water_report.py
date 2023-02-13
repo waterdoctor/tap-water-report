bf18a98e07aad34b45f3e17891753a8a3a930795
Timoh <timscoh@gmail.com>
2023-02-13 14:49:09 -0500

    bug fix

--- water_report.py
+++ water_report.py
@@ -51,14 +51,14 @@ def inject_ga():
 
     # Insert the script in the head tag of the static template inside your virtual
     index_path = Path(st.__file__).parent / "static" / "index.html"
-    logging.info(f'editing {index_path}')
     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
     if not soup.find(id=GA_ID): 
         bck_index = index_path.with_suffix('.bck')
         if bck_index.exists():
             shutil.copy(bck_index, index_path)  
         else:
-            shutil.copy(index_path, bck_index)  
+            shutil.copy(index_path, bck_index)
+        logging.info(f'editing {index_path}')
         html = str(soup)
         new_html = html.replace('<head>', '<head>\n' + GA_JS)
         index_path.write_text(new_html)
