5c3601710d6e32404441b265cbde0d7a371fb1c9
Timoh <timscoh@gmail.com>
2023-02-13 14:06:44 -0500

    inject google analytics

--- water_report.py
+++ water_report.py
@@ -33,6 +33,38 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+# -------- GOOGLE ANALYTICS --------
+def inject_ga():
+    GA_ID = "google_analytics"
+
+    GA_JS = """
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZQ527HHEBC"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-ZQ527HHEBC');
+    </script>
+    """
+
+    # Insert the script in the head tag of the static template inside your virtual
+    index_path = Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID): 
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  
+        else:
+            shutil.copy(index_path, bck_index)  
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
+        index_path.write_text(new_html)
+
+
+inject_ga()
 
 # -------- SETTINGS --------
 vert_space = '<div style="padding: 10px 5px;"></div>'
